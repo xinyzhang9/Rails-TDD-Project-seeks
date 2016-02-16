b0b5c3342897641216f45392d643af423c3638ce
Xinyang <xinyzhang9@gmail.com>
2016-02-15 17:16:51 -0800

    add model:secrets and pass relationship test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :secrets
+  has_many :likes,dependent: :destroy
+  has_many :secrets_liked,through: :likes,source: :secret
+
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :name,presence:true
   validates :email,presence:true,format:{with:EMAIL_REGEX},uniqueness:{case_sensitive:false}
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215083849) do
+ActiveRecord::Schema.define(version: 20160216005140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "secret_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "likes", ["secret_id"], name: "index_likes_on_secret_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
+
+  create_table "secrets", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "secrets", ["user_id"], name: "index_secrets_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
