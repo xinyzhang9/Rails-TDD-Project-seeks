b147dcb3fa8cdde30940fa21ed63f151e7d02efb
Xinyang <xinyzhang9@gmail.com>
2016-02-15 23:24:39 -0800

    pass test:secrets#create

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,4 +2,17 @@ class SecretsController < ApplicationController
   def index
   	@secrets = Secret.all
   end
+
+  def create
+  	user_id = params[:id]
+  	@user = User.find(user_id)
+  	secret = Secret.new(content:params[:content],user:@user)
+  	if secret.valid?
+  		secret.save
+  		redirect_to @user
+  	else
+  		flash[:errors] = secret.errors.full_messages
+  		redirect_to @user
+  	end
+  end
 end
